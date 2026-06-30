6214fd662a81ee46dc8b59e26c463d37eb3e9b7e
gjromero6906 <gjr6906@gmail.com>
2026-06-30 19:04:56 -0400

    login/makeAccount

--- server/app/db/models.py
+++ server/app/db/models.py
@@ -8,6 +8,8 @@ class User(Base):
     __tablename__ = 'users'
 
     user_id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(Text, nullable=False)
